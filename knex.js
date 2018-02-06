2cab092ab5e5ccba4666774ba0c96e9c7c639e4d
alishaevn <alieva.inc@gmail.com>
2018-02-06 13:14:27 -0800

    converted notes.filter into knex

--- knex.js
+++ knex.js
@@ -4,4 +4,5 @@ const knexConfig = require('./knexfile');
 
 const environment = process.env.NODE_ENV || 'development';
 
-module.exports = require('knex')(knexConfig[environment]);
+module.exports = require('knex')(knexConfig[environment]); 
+// requiring knex creates a function, but what does it mean that knexConfig && environment are passed in as one argument like this?
