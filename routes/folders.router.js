7a87b4771939a5dfb91d578bcf0df93cdfa11c50
alishaevn <alieva.inc@gmail.com>
2018-02-07 13:42:54 -0800

    can access all notes && folders at /v2/notes

--- routes/folders.router.js
+++ routes/folders.router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 /* Notes can belong to a folder because we've altered the notes table. However, creating this file is what will allow the folders to be viewed from the browser */
 
 /* ===== GET ALL FOLDERS ===== */
-router.get('/folders', (req, res, next) => {
+router.get('/notes', (req, res, next) => {
 
   knex.select('id', 'name')
     .from('folders')
@@ -21,24 +21,27 @@ router.get('/folders', (req, res, next) => {
 
 
 /* ===== GET FOLDER BY ID ===== */
-router.get('/folders/:id', (req, res, next) => {
+router.get('/notes/:id', (req, res, next) => {
 
 });
 
 
 /* ===== FOLDER UPDATE ===== */
-router.put('/folders/:id', (req, res, next) => {
+router.put('/notes/:id', (req, res, next) => {
 
 });
 
 
 /* ===== CREATE A FOLDER ===== */
-router.post('/folders', (req, res, next) => {
+router.post('/notes', (req, res, next) => {
 
 });
 
 
 /* ===== DELETE A FOLDER ===== */
-router.delete('/folders/:id', (req, res, next) => {
+router.delete('/notes/:id', (req, res, next) => {
 
 });
+
+
+module.exports= router;
