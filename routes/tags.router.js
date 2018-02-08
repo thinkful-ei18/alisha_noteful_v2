7d3f04fe267ccf7f4c435430f0a0249533374d8b
alishaevn <alieva.inc@gmail.com>
2018-02-08 09:42:09 -0800

    required files for tags.router && created outline code for 5 routes

---
+++ routes/tags.router.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const knex = require('../knex');
+const express = require('express');
+
+const router = express.Router();
+
+
+/* ========== GET/READ ALL TAGS ========== */
+router.get('/tags', (req, res, next) => {
+  
+});
+
+/* ========== GET/READ SINGLE TAG ========== */
+router.get('/tags/:id', (req, res, next) => {
+  
+
+});
+
+/* ========== PUT/UPDATE A SINGLE TAG ========== */
+router.put('/tags/:id', (req, res, next) => {
+
+
+});
+
+/* ========== POST/CREATE TAG ========== */
+router.post('/tags', (req, res, next) => {
+  
+  
+});
+
+/* ========== DELETE/REMOVE A SINGLE TAG ========== */
+router.delete('/tags/:id', (req, res, next) => {
+  
+});
+
+
+module.exports = router;
